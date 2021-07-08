a3467074c2eb262d1bb7b3fc1cd7956726ff89c5
rio-vps <testuser01@defomate.net>
2021-07-09 08:50:19 +0900

    ニックネームのカラムと入力フォームを追加

--- db/migrate/20210625224433_add_confirmation_sent_at_to_users.rb
+++ db/migrate/20210625224433_add_confirmation_sent_at_to_users.rb
@@ -1,5 +1,5 @@
 class AddConfirmationSentAtToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :confirmation_sent_at, :datetime
+    # add_column :users, :confirmation_sent_at, :datetime
   end
 end
--- db/migrate/20210704101531_rename_username_column_to_nickname.rb
+++
@@ -1,5 +0,0 @@
-class RenameUsernameColumnToNickname < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :users, :username, :nickname
-  end
-end
