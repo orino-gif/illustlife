bae7b288d60254d080c7a34be9de89b34bb0cc1f
rio-vps <testuser01@defomate.net>
2021-08-06 08:21:49 +0900

    S3で画像が保存できる機能を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.default_url_options = { host: 'illustlife.com' }
-  config.active_storage.service = :local
-  #config.active_storage.service = :amazon
+  #config.active_storage.service = :local
+  config.active_storage.service = :amazon
   
   # config/environments/{staging,development}.rb
   config.action_mailer.delivery_method = :ses
