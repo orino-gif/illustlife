07174b854d64ab28b9bf2e508b502a73984e78a1
rio-vps <testuser01@defomate.net>
2021-08-22 05:42:30 +0900

    プロフィール画面にヘッダー画像を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,11 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
-    
-    
   end
   
   def show
-    # @user = User.find(params[:id])
+    @users = User.all
     @user = User.find_by(id:params[:id])
     @creator = Creator.find_by(user_id:@user.id)
-    #logger.debug{@creator.name}
   end
 end
