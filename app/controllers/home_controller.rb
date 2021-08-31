a0f2d4453b09c3f82eecea2959201e3d5f375548
rio-vps <testuser01@defomate.net>
2021-08-31 12:01:42 +0900

    ファビコンを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,6 @@ class HomeController < ApplicationController
   end
   
   def show
-    @users = User.all
     @user = User.find_by(id:params[:id])
     @creator = Creator.find_by(user_id:@user.id)
   end
