13e5d1ad433c0162cbb823976a99a5d7977bc321
rio-vps <testuser01@defomate.net>
2021-09-02 23:44:46 +0900

    creatorテーブルのidカラムを削除

--- db/migrate/20210803085238_create_creators.rb
+++ db/migrate/20210803085238_create_creators.rb
@@ -1,13 +1,14 @@
 class CreateCreators < ActiveRecord::Migration[5.2]
   def change
-    create_table :creators do |t|
+    create_table :creators, id: false do |t|
+      t.column :user_id, 'INTEGER PRIMARY KEY NOT NULL'
       t.string :icon
       t.string :header
-      t.references :user
+      
       
       t.timestamps
     end
     # 親テーブルに対象が存在しない場合、子テーブルへの保存ができないようにする。
-    add_foreign_key :creators, :users
+    #add_foreign_key :creators, :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,13 +33,11 @@ ActiveRecord::Schema.define(version: 2021_08_03_085238) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "creators", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "creators", primary_key: "user_id", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "icon"
     t.string "header"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_creators_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -60,5 +58,4 @@ ActiveRecord::Schema.define(version: 2021_08_03_085238) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "creators", "users"
 end
