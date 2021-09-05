9515413ec4cc22285d5337b7df663cffb6dc742b
rio-vps <testuser01@defomate.net>
2021-09-05 18:55:04 +0900

    ディフォルメキャラの画像を3つ追加できるように変更

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -2,6 +2,8 @@ class Creator < ApplicationRecord
   mount_uploader :header, CreatorUploader
   mount_uploader :icon, CreatorUploader
   mount_uploader :deforume1, CreatorUploader
+  mount_uploader :deforume2, CreatorUploader
+  mount_uploader :deforume3, CreatorUploader
   belongs_to :user, optional: true #optional:trueは外部キーを許可する設定
 end
 
