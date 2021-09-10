297c35f7e38200af4660e08b4390a1df5fb8225a
rio-vps <testuser01@defomate.net>
2021-09-11 08:50:34 +0900

    ディフォルメキャラのコードを簡略化(テスト)

--- app/helpers/creators_helper.rb
+++ app/helpers/creators_helper.rb
@@ -1,5 +1,14 @@
 module CreatorsHelper
-    def display_nickname(name)
-        "ニックネーム：#{name}"
-    end
+  def display_nickname(name)
+      "ニックネーム：#{name}"
+  end
+  
+  def display_image(image_name)
+
+   if @creator.deforume1?
+        image_tag @creator.deforume1.url, class: 'thumbnail' 
+   else 
+        image_tag '/img/noImage.png', class: 'thumbnail cover' 
+   end 
+  end
 end
