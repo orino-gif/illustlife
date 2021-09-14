b60a66d6b72d96775f7aa7792d39bd99dababf58
rio-vps <testuser01@defomate.net>
2021-09-15 04:31:57 +0900

    メーラーを異なるユーザ毎に利用できるようにする

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,11 @@
 class UserMailer < ApplicationMailer
   default from: 'xr274375@fc5.so-net.ne.jp'
 
-  def welcome_email
-    mail(to: "orino0710@gmail.com", subject: '私の素敵なサイトへようこそ')
+  def welcome_email(user)
+    @user = user
+    mail(
+        to: @user.email,
+        subject: '私の素敵なサイトへようこそ'
+      )
   end
 end
