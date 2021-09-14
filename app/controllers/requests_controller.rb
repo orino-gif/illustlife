f6766f89db0a4484dc0aad3db9fc83f6599a4cd0
rio-vps <testuser01@defomate.net>
2021-09-14 19:06:55 +0900

    リクエスト状態に受信者と送信者のニックネームを追加

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,7 +2,7 @@ class RequestsController < ApplicationController
   before_action :authenticate_user!
   def new
     @requests = Request.new
-     $receive_id=params[:id]
+    $receive_id=params[:id]
      #@send_id= current_user.id
     # @requests.save
     @user = User.find(params[:id])
@@ -14,13 +14,22 @@ class RequestsController < ApplicationController
     @creator = Creator.find(current_user.id)
     @requests.receive_id=$receive_id.to_i
     @requests.send_id=current_user.id.to_i
-    @requests.status="START"
+    @requests.status="承認待ち"
     if @requests.save!
+      $requests_id = @requests.id
       redirect_to request_url(@requests.send_id), notice: 'リクエストを送信しました。'
       #redirect_to root_url, notice: 'リクエストを送信しました。'
     else
       render :new
     end
+  end
+  
+  def show
+    @requests = Request.find($requests_id)
+    @sender = User.find(@requests.send_id)
+    @receiver = User.find(@requests.receive_id)
+    
+    
   end
   
   def update
