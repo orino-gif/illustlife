0613eeb3d50b9e9b6241a967a5040e127f74d804
rio-vps <testuser01@defomate.net>
2021-09-22 21:05:28 +0900

    index.htmlのコードをリファクタリング

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,5 +1,9 @@
 module HomeHelper
-  def display_character()
-    p link_to image_tag('/img/mobu.png',class: 'icon'), "/creators/#{creator.user_id}"
+  def display_img(creator)
+    if creator.icon?
+      link_to image_tag(creator.icon.url, class: 'icon center-block'), "/creators/#{creator.user_id}" 
+    else
+     link_to image_tag('/img/mobu.png', class: 'icon center-block'), "/creators/#{creator.user_id}" 
+    end
   end
 end
