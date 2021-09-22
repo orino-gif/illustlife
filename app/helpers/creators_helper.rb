ccb29fb9bd4f4da00c8a38729cf8f9b4846ffd60
rio-vps <testuser01@defomate.net>
2021-09-22 10:12:14 +0900

    ユーザー情報表示のコードを無駄なく美しくリファクタリング

--- app/helpers/creators_helper.rb
+++ app/helpers/creators_helper.rb
@@ -1,12 +1,36 @@
 module CreatorsHelper
 
-  def display_image(creator)
+  def display_fg(creator)
     if creator == @creator.fg1 && @creator.fg1?
       image_tag @creator.fg1.url, class: 'thumbnail'
     elsif creator == @creator.fg2 && @creator.fg2?
       image_tag @creator.fg2.url, class: 'thumbnail'
     elsif creator == @creator.fg3 && @creator.fg3?
       image_tag @creator.fg3.url, class: 'thumbnail'
+    else
+      image_tag '/img/noImage.png', class: 'thumbnail cover'
+    end
+  end
+  
+  def display_bg(creator)
+    if creator == @creator.bg1 && @creator.bg1?
+      image_tag @creator.bg1.url, class: 'thumbnail'
+    elsif creator == @creator.bg2 && @creator.bg2?
+      image_tag @creator.bg2.url, class: 'thumbnail'
+    elsif creator == @creator.bg3 && @creator.bg3?
+      image_tag @creator.bg3.url, class: 'thumbnail'
+    else
+      image_tag '/img/noImage.png', class: 'thumbnail cover'
+    end
+  end
+  
+  def display_cg(creator)
+    if creator == @creator.cg1 && @creator.cg1?
+      image_tag @creator.cg1.url, class: 'thumbnail'
+    elsif creator == @creator.cg2 && @creator.cg2?
+      image_tag @creator.cg2.url, class: 'thumbnail'
+    elsif creator == @creator.cg3 && @creator.cg3?
+      image_tag @creator.cg3.url, class: 'thumbnail'
     else
       image_tag '/img/noImage.png', class: 'thumbnail cover'
     end
