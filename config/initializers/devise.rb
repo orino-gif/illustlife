1767008ff31fdac9a7fa4d9c7f799300af0c2679
rio-vps <testuser01@defomate.net>
2021-09-26 19:32:17 +0900

    ログアウト出来ない不具合を修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,8 +266,11 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  ENV['SIGN_OUT_VIA']
-  #config.sign_out_via = :delete
+  if Rails.env.production?
+    config.sign_out_via = :get
+  else
+    config.sign_out_via = :delete
+  end
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
