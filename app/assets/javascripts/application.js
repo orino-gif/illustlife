623b64310978e41b19b6695e3c04b5f990337798
rio-vps <testuser01@defomate.net>
2021-10-10 09:55:51 +0900

    jQueryライブラリの呼び出し順序を変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
-//= require jquery
-//= require jquery_ujs
+
 
 src="https://code.jquery.com/jquery-3.2.1.min.js"
 src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"
