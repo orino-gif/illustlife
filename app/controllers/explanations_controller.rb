f8156dd27f0c9bc9efd18f317292c7948b5b09ea
rio-vps <testuser01@defomate.net>
2022-02-27 11:34:52 +0900

    意見フォームを設置

--- app/controllers/explanations_controller.rb
+++ app/controllers/explanations_controller.rb
@@ -1,19 +1,23 @@
 class ExplanationsController < ApplicationController
   def index
+    @explanations = Explanation.all
   end
   def new
     @explanations = Explanation.new
   end
   
   def create
-    @explanations = Explanation.new
+    @explanations = Explanation.new(explanations_params)
     if @explanations.save
       redirect_to explanations_url, notice: '運営へフォームの内容を送信しました。'
     else
       render :new
     end
   end
+
+  private
   
-  def show
-  end
+  def explanations_params
+    params.require(:explanation).permit(:letter_body)
+  end 
 end
