aebe27725f4f963d622e7d4fdc4c0fc8e68d6ab9
rio-vps <testuser01@defomate.net>
2022-04-04 00:33:40 +0900

    wheneverを追可2

--- lib/script/expired_notice.rb
+++ lib/script/expired_notice.rb
@@ -1,9 +1,19 @@
-class Expired
+class Expired 
   def self.notice
-    date = Date.today-1
+    date = Date.today-3
     p date
-    @requests = Request.where("created_at >= ? AND created_at < ?", date, date + 1)
+    @requests = Request.where("created_at >= ? AND created_at < ?", date, date + 1).where(status:'承認待ち')
     p @requests
+    @requests.each do |request| 
+      request.status = '期限切れ'
+      sender = User.find_by(nickname: request.sender)
+      p sender
+      receiver = User.find_by(nickname: request.receiver)
+      p receiver
+      UserMailer.refusal_email(sender, receiver, request).deliver_later
+
+      request.save
+    end
   end
 end
 
