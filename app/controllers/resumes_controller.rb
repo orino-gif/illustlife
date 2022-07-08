b04df5e8d91d11b83d02e9cd1b7dec38ecc032dc
rio-vps <testuser01@defomate.net>
2022-07-08 11:07:15 +0900

    再開通知リストでのバグを修正

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -13,12 +13,12 @@ class ResumesController < ApplicationController
     @resume.resume_user =params[:resume][:resume_user]
     @resume.notification_user = current_user.id
     if @resume.save
-      redirect_to request.referer, notice: '再開通知を設定しました'
+      redirect_to resume_path(current_user.id), notice: '再開通知を設定しました'
     end
   end
   
   def show
-    @resumes = Resume.where(resume_user:current_user.id)
+    @notification_users = Resume.where(notification_user:current_user.id)
     if not params[:record_delete].nil?
       Resume.find(params[:record_delete]).delete
     end
