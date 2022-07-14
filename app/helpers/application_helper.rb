0259dbfb730caf9035519b5da03ef209e5921f32
rio-vps <testuser01@defomate.net>
2022-07-14 20:05:33 +0900

    ユーザー情報編集のレイアウトを変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def display_icon(creator, appearance)
     if creator.icon?
-      if not (controller.controller_name == 'creators') && (controller.action_name == 'show')
+      if not (controller.controller_name == 'creators') && ((controller.action_name == 'show') || (controller.action_name == 'edit'))
         link_to image_tag(creator.icon.url, class: appearance), creator_path(creator.user_id)
       else
         image_tag creator.icon.url, class: appearance
       end
     else
-      if not (controller.controller_name == 'creators') && (controller.action_name == 'show')
+      if not (controller.controller_name == 'creators') && ((controller.action_name == 'show') || (controller.action_name == 'edit'))
         link_to image_tag('/img/mobu.png', class: appearance), creator_path(creator.user_id)
       else
         image_tag '/img/mobu.png', class: appearance
