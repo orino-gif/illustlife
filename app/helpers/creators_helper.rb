245be6ee7f56f9248720201b4761a91281e4977a
rio-vps <testuser01@defomate.net>
2022-07-16 12:56:28 +0900

    インデックスのアイコン画像をヘッダーに変更

--- app/helpers/creators_helper.rb
+++ app/helpers/creators_helper.rb
@@ -1,9 +1,17 @@
 module CreatorsHelper
   def display_header(creator, appearance)
-    if creator.header?
-      image_tag creator.header.url, class:appearance
+    if 'homes' == controller.controller_name
+      if creator.header?
+        link_to image_tag(creator.header.url, class: appearance), creator_path(creator.user_id)
+      else
+        link_to image_tag('/img/nothing.png', class: appearance), creator_path(creator.user_id)
+      end
     else
-      image_tag '/img/nothing.png', class:appearance
+      if creator.header?
+        image_tag creator.header.url, class: appearance
+      else
+        image_tag '/img/nothing.png', class: appearance
+      end
     end
   end
 end
