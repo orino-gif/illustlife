7c61847f50b101ab8b08f2621cc2ad5b409bd11d
rio-vps <testuser01@defomate.net>
2022-07-26 21:42:29 +0900

    ボタンのレイアウトを変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,9 @@
 module ApplicationHelper
   def display_icon(creator, appearance)
     if creator.icon?
-      if not ('creators' == controller.controller_name ) && (('show' == controller.action_name ) || ('edit' == controller.action_name))
-        link_to image_tag(creator.icon.url, class: appearance), creator_path(creator.user_id)
-      else
-        image_tag creator.icon.url, class: appearance
-      end
+      link_to image_tag(creator.icon.url, class: appearance), creator_path(creator.user_id)
     else
-      if not ('creators' == controller.controller_name ) && (('show' == controller.action_name ) || ('edit' == controller.action_name))
-        link_to image_tag('/img/mobu.png', class: appearance), creator_path(creator.user_id)
-      else
-        image_tag '/img/mobu.png', class: appearance
-      end
+      link_to image_tag('/img/mobu.png', class: appearance), creator_path(creator.user_id)
     end
   end
 end
