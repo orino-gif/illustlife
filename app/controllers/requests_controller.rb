d2e23bdc8a8203bfeacd29a9038dc86c4b7678d5
rio-vps <testuser01@defomate.net>
2022-08-08 14:03:51 +0900

    平均納品日数の表示機能を先送り2

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -59,7 +59,7 @@ class RequestsController < ApplicationController
       end
 
       if 0 != @receiver.creator.number_of_approval
-        # @receiver.creator.deadline_strict_adherence_rate = 100-(@requests.all.sum(:is_in_time_for_the_deadline) / @receiver.creator.number_of_approval)
+        @receiver.creator.deadline_strict_adherence_rate = 100
       end
       @request.status = params[:status]
       @request.save
