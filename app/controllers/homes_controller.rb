90a66107d9c7da6ed58c16d0748ddc965d7833eb
rio-vps <testuser01@defomate.net>
2022-08-10 10:50:37 +0900

    リクエストの表示順序を変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,6 @@ class HomesController < ApplicationController
 
   def index
     @creators = Creator.all.order(evaluation_points: 'DESC').order(created_at: 'DESC')
-    @requests = Request.all
+    @requests = Request.where(status: '納品完了').order(updated_at: 'DESC')
   end
 end
