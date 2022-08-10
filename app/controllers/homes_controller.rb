9e29af1da30a84f1ee87b78111d4b616147268e7
rio-vps <testuser01@defomate.net>
2022-08-10 23:39:52 +0900

    ヘッダーの画像設定がされていない場合は表示しない

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 
   def index
-    @creators = Creator.all.order(evaluation_points: 'DESC').order(created_at: 'DESC')
+    @creators = Creator.where.not(header: nil).order(evaluation_points: 'DESC').order(created_at: 'DESC')
     @requests = Request.where(status: '納品完了').order(updated_at: 'DESC')
   end
 end
