8d182efebac91792b6fd8d7ab16a9e42d633bd5f
rio-vps <testuser01@defomate.net>
2022-09-07 02:45:12 +0900

    creator関連のメンテナンス性の向上4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   # アイコンを表示。showアクション以外はアイコンにリンク先を付与
   def display_icon(creator, appearance)
-    if 'show' != controller.action_name
-      
+    if 'creotors' != controller.controller_name \
+      && 'show' != controller.action_name
       # creator.iconの値がNULLなら代替の画像を表示
       if creator.icon?
         link_to image_tag(creator.icon.url, class: appearance),
