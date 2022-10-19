7c14995d1857c50eb28565bd0b94754a20ca2925
rio-vps <testuser01@defomate.net>
2022-10-19 22:04:26 +0900

    プレビューの機能を追加2

--- app/assets/javascripts/exposures.js
+++ app/assets/javascripts/exposures.js
@@ -3,11 +3,19 @@
 //  You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on("change",".preview-uploader",function(){
-    let elem = this                                             //操作された要素を取得
-    let fileReader = new FileReader();                          //ファイルを読み取るオブジェクトを生成
-    fileReader.readAsDataURL(elem.files[0]);                    //ファイルを読み取る
-    fileReader.onload = (function () {                          //ファイル読み取りが完了したら
-        let imgTag = `<img class="fit_contain size_medium-rectangle" $Xlarge; src='${fileReader.result}'>`         //img要素を生成
-        $(elem).next(".preview").html(imgTag)               //画像をプレビュー
+    //操作された要素を取得
+    let elem = this
+    //ファイルを読み取るオブジェクトを生成
+    let fileReader = new FileReader();
+    //ファイルを読み取る
+    fileReader.readAsDataURL(elem.files[0]);
+    //ファイル読み取りが完了したら
+    fileReader.onload = (function () {
+      //img要素を生成
+      let imgTag = 
+      `<img class="fit_contain size_medium-rectangle"
+      $Xlarge; src='${fileReader.result}'>`
+      //画像をプレビュー
+      $(elem).next(".preview").html(imgTag)
     });
 })
