cb782ad0862655f2f814b97bcfeff403cdb9d997
rio-vps <testuser01@defomate.net>
2022-11-01 00:04:06 +0900

    カードのテーブルのカラム名を変更2

--- db/migrate/20221031120014_change_credits.rb
+++ db/migrate/20221031120014_change_credits.rb
@@ -1,6 +1,6 @@
 class ChangeCredits < ActiveRecord::Migration[5.2]
   def change
-    rename_column :credits, :branch, :branch
+    rename_column :credits, :branch_name, :branch
     rename_column :credits, :type, :account_type
     rename_column :credits, :account_number, :number
     rename_column :credits, :account_holder, :holder
