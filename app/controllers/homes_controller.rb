ce6c826693f78162db0f140c95b8b7f32c8b9ad5
rio-vps <testuser01@defomate.net>
2022-12-01 19:03:11 +0900

    クリエーターを評価順に並び変える2

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def index
-    @cres = Cre.joins(:pfm).order(evl: 'DESC')
+    @cres = Cre.joins(:pfm,:user).order(evl: 'DESC', created_at: 'DESC')
     p @cres
   end
 end
