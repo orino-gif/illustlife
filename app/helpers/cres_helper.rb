97f8f36847cb9f9bde740a1002743f9c7bf39521
rio-vps <testuser01@defomate.net>
2022-12-01 21:08:44 +0900

    トップにアイコンを追加4

--- app/helpers/cres_helper.rb
+++ app/helpers/cres_helper.rb
@@ -1,7 +1,7 @@
 module CresHelper
   # クリエーターのリンク先が付与されたSNSアイコンを表示
   def disp_sns(cre, sns)
-    if cre.send(sns).present? && (not cre.send(sns).nil?)
+    if cre.send(sns).present? && 'NULL' != cre.send(sns)
       link_to image_tag("/img/#{sns}.png", class: 'ss_square s_ta-pos'),
       cre.send(sns)
     end
