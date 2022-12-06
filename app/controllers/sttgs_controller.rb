21e25437cd205192dabb0ff94fe2c08540d49174
rio-vps <testuser01@defomate.net>
2022-12-06 13:48:45 +0900

    リクエスト送信ボタンをdisable2

--- app/controllers/sttgs_controller.rb
+++ app/controllers/sttgs_controller.rb
@@ -2,7 +2,7 @@ class SttgsController < ApplicationController
   def update
     @sttg =  Sttg.find_by(cre_id: params[:id])
     if @sttg.update(sttgs_params)
-      redirect_to cre_path(params[:id]), notice: '設定情報を更新しました。'
+      redirect_to "/reqs/#{params[:id]}/new", notice: '設定情報を更新しました。'
     end
   end
   
