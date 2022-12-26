2f2ab28731e282c72e1a05e71a6b8c8904aeae65
rio-vps <testuser01@defomate.net>
2022-12-26 21:21:16 +0900

    晒す機能追加後修正3

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,10 +2,6 @@ class HomesController < ApplicationController
   def index
     @cres = Cre.joins(:pfm,:user).order(evl: 'DESC', created_at: 'DESC')
     @ovrs = Ovr.all
-    if not Ovr.where.not("expor_id LIKE ?","%*%").empty?
-      @expors = Expor.joins(:ovrs).merge(Ovr.where.not("expor_id LIKE ?","%*%"))
-    else
-      @expors = Expor.all
-    end
+    @expors = Expor.where.not(id: Ovr.all.select(:expor_id))
   end
 end
