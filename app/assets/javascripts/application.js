0b19256dcd200c3fcf0eeea74bc9ac0eecdbfb60
rio-vps <testuser01@defomate.net>
2023-03-06 21:06:47 +0900

    jQueryの呼び出しコードを変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,44 +12,7 @@
 //
 //= require rails-ujs
 //= require jquery
-//= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
-//= require chartkick
-//= require Chart.bundle
 //= require_tree .
 
-src="https://code.jquery.com/jquery-3.2.1.min.js"
-src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"
-
 console.log("jQuery稼働OK");
-
-$(document).on("change",".preview",function(){
-  //操作された要素を取得
-  let elem = this
-  //ファイルを読み取るオブジェクトを生成
-  let fileReader = new FileReader();
-  //ファイルを読み取る
-  fileReader.readAsDataURL(elem.files[0]);
-  //ファイル読み取りが完了したら
-  fileReader.onload = (function () {
-    //img要素を生成
-    let lll_img = `<img class="lll_square fit_con"
-    $Xlarge; src='${fileReader.result}'>`
-
-    //画像をプレビュー
-    $(elem).next(".lll_img,.edit_img").html(lll_img)
-  });
-})
-
-// $(function () {
-//   $('.dbl').mouseover(function() {
-//       // ここに処理を記述する
-//       $(this).css('transform','scale(2)');
-  
-//   })
-//   $('.dbl').mouseout(function() {
-//       // ここに処理を記述する
-//       $(this).css('transform','scale(1)');
-//   })
-// });
--- app/assets/javascripts/ttls.js
+++ app/assets/javascripts/ttls.js
@@ -1,3 +1,20 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).on("change",".preview",function(){
+  //操作された要素を取得
+  let elem = this
+  //ファイルを読み取るオブジェクトを生成
+  let fileReader = new FileReader();
+  //ファイルを読み取る
+  fileReader.readAsDataURL(elem.files[0]);
+  //ファイル読み取りが完了したら
+  fileReader.onload = (function () {
+    //img要素を生成
+    let lll_img = `<img class="lll_square fit_con"
+    $Xlarge; src='${fileReader.result}'>`
+
+    //画像をプレビュー
+    $(elem).next(".lll_img,.edit_img").html(lll_img)
+  });
+})
